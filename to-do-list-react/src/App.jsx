7f7b0a126a21629d5857b71541ae4cc131bec7fb
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-03-21 12:10:00 -0300

    atualizado

---
+++ to-do-list-react/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Todo from "./assets/Todo";
+import './App.css'
+
+function App() {
+  
+  return ( 
+  <Todo/>
+  
+  )
+}
+
+export default App
---
+++ to-do-list-react/src/assets/Todo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Todo() {
+
+return (
+    <div>
+        <h1>ola</h1>
+    </div>
+)
+
+
+}
+
+export default Todo
