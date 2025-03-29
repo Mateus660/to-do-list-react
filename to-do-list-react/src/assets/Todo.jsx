70703ee760acd801914ab13900d35cdcac3ec364
Mateus Emanuel <145380377+Mateus660@users.noreply.github.com>
2025-03-29 19:38:42 -0300

    pronto

--- to-do-list-react/src/assets/Todo.jsx
+++ to-do-list-react/src/assets/Todo.jsx
@@ -1,20 +1,38 @@
 import React from "react";
 
 function Todo() {
+    return (
+        <div id="container">
+            {/* Título centralizado no topo */}
+            <h1 id="titulo" style={{ textAlign: "center", marginTop: "20px" }}>
+                Lista de Tarefas
+            </h1>
 
-return (
-    <div id="container">
-        <div id="adtarefa">
-            <input type="text" name="tarefa" id="colocar-tarefa" />
-            <button type="submit" id="adicionar"><p>Adicionar</p></button>
-        </div> 
-        <div id="listatarefa">
-            <h1>tarefas</h1>
+            {/* Área de entrada */}
+            <div id="area-input" style={{ textAlign: "center", marginTop: "20px" }}>
+                <input 
+                    type="text" 
+                    name="tarefas" 
+                    id="inserir-tarefa" 
+                    placeholder="Insira sua tarefa" 
+                />
+                <input type="submit" value="Adicionar" id="adicionar" />
+            </div> 
+
+            {/* Lista de tarefas */}
+            <div id="lista-tarefas" style={{ margin: "20px auto", width: "50%", textAlign: "center" }}>
+                <h2>Tarefas</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda minima qui dignissimos 
+                    velit aut hic cumque animi maxime ea magnam.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium tempore dolor voluptate 
+                    ipsam velit minima reprehenderit necessitatibus.
+                </p>
+            </div>
         </div>
-    </div> 
-)
-
-
+    );
 }
 
-export default Todo
+export default Todo;
